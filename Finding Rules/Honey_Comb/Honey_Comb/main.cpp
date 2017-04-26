de105983725d64f30d1398641c11bd48512a5982
Ted <kimtx2017@gmail.com>
2017-04-26 14:18:10 +0900

    # Solve Problem

--- Finding Rules/Honey_Comb/Honey_Comb/main.cpp
+++ Finding Rules/Honey_Comb/Honey_Comb/main.cpp
@@ -4,12 +4,35 @@
 //
 //  Created by Ted on 2017. 4. 26..
 //  Copyright © 2017년 Ted. All rights reserved.
-//
+
+using namespace std;
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    long long int sum=1, n=1;
+    long long int target;
+    
+    cin >> target;
+    
+    while(true){
+        
+        sum += 6*n;
+        
+        if(target == 1){
+            cout << target << endl;
+            break;
+        }
+        else if(sum>=target){
+            cout << n+1 << endl;
+            break;
+        }
+        else
+            n++;
+
+    }
+    
     return 0;
 }
+
