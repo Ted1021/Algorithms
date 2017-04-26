a114dcc0ffdd6601d5511aa8185d81f25012b2c0
Ted <kimtx2017@gmail.com>
2017-04-26 10:15:50 +0900

    # Create Project - HoneyComb

---
+++ Finding Rules/Honey_Comb/Honey_Comb/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Honey_Comb
+//
+//  Created by Ted on 2017. 4. 26..
+//  Copyright © 2017년 Ted. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
