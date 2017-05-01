03ce1189e5beaf0dedf5762860e65176240281dc
Ted <kimtx2017@gmail.com>
2017-05-01 17:18:44 +0900

    # Create Problem
    - Goldbach's conjecture

---
+++ Prime Number/9020_Goldbach_Conjecture/Goldbach's conjecture/Goldbach's conjecture/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Goldbach's conjecture
+//
+//  Created by Ted on 2017. 5. 1..
+//  Copyright © 2017년 Ted. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+  
+    
+    
+    return 0;
+}
