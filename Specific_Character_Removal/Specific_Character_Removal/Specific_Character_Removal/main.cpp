8772189c23885541377e475d3d53ba646420360f
Ted <kimtx2017@gmail.com>
2017-06-14 17:24:04 +0900

    # Solve Probelm
    - use replace instead of remove
    - replace target character to ' '

--- Specific_Character_Removal/Specific_Character_Removal/Specific_Character_Removal/main.cpp
+++ Specific_Character_Removal/Specific_Character_Removal/Specific_Character_Removal/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,13 +15,24 @@ string removeChars(string str, string remove);
 
 int main(int argc, const char * argv[]) {
     
+    string str, remove;
+    
+    cout << "Insert target String : " << endl;
+    getline(cin, str);
+    
+    cout << "Insert remove characters you want" << endl;
+    cin >> remove;
+    
+    cout << removeChars(str, remove) << endl;
     
     return 0;
 }
 
 string removeChars(string str, string remove){
     
+    for(char c : remove){
+        replace(str.begin(), str.end(), c, ' ');
+    }
     
-    
-    return NULL;
+    return str;
 }
